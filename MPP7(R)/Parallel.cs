85fe88fe0af3c41352764fdbb08eb12f1406d77f
SingleArtist <95147229+SingleArtist@users.noreply.github.com>
2021-12-29 20:57:32 +0300

    Add files via upload

---
+++ MPP7(R)/Parallel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+//Задание 7
+/*Создать на языке C# статический метод класса Parallel.WaitAll,
+который:
+-принимает в параметрах массив делегатов; -выполняет все указанные делегаты параллельно с помощью пула
+потоков;
+-дожидается окончания выполнения всех делегатов.
+Реализовать простейший пример использования метода
+Parallel.WaitAll.*/
+
+namespace SeventhTask
+{
+    class Parallel
+    {
+        public static void WaitAll(WaitCallback[] tasks)
+        {
+            foreach (var task in tasks)
+                ThreadPool.QueueUserWorkItem(task);
+
+            int availableThreads;
+            int maxNumThread;
+            ThreadPool.GetMaxThreads(out maxNumThread, out _);
+            do
+            {
+                ThreadPool.GetAvailableThreads(out availableThreads, out _);
+            } while (maxNumThread != availableThreads);
+        }
+    }
+}
---
+++ MPP7(R)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+//Задание 7
+/*Создать на языке C# статический метод класса Parallel.WaitAll,
+который:
+-принимает в параметрах массив делегатов; -выполняет все указанные делегаты параллельно с помощью пула
+потоков;
+-дожидается окончания выполнения всех делегатов.
+Реализовать простейший пример использования метода
+Parallel.WaitAll.*/
+
+namespace SeventhTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback[] tasks = CreateTasks(10);
+            Parallel.WaitAll(tasks);
+        }
+
+        private static WaitCallback[] CreateTasks(int taskNumber)
+        {
+            WaitCallback[] tasks = new WaitCallback[taskNumber];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = obj =>
+                {
+                    Thread.Sleep(100);
+                    Console.WriteLine($"Task in {Thread.CurrentThread.ManagedThreadId} completed.");
+                };
+            }
+            return tasks;
+        }
+    }
+}
